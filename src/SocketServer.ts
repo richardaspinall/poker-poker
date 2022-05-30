03fa3fb76f3bd797a435fccac8d66d96450880f0
Richard Aspinall <rls.aspinall@gmail.com>
2022-05-30 14:10:56 +1000

    Add sessions

--- src/SocketServer.ts
+++ src/SocketServer.ts
@@ -1,3 +1,4 @@
+import express, { Request, Response, NextFunction } from 'express';
 import { Server as HttpServer } from 'http';
 import { Server as IOServer, Socket } from 'socket.io';
 import registerTableEvents from './TableHandlers';
@@ -6,13 +7,28 @@ export default class SocketServer {
   private static io: IOServer;
 
   // Creates our Socket Server and registers appropriate events
-  public static initialize(httpServer: HttpServer) {
+  public static initialize(httpServer: HttpServer, sessionMiddleware: express.RequestHandler) {
     if (!this.io) {
       this.io = new IOServer(httpServer, {
         /* options */
       });
 
+      this.io.use((socket, next) => {
+        sessionMiddleware(socket.request as Request, {} as Response, next as NextFunction);
+      });
+
+      this.io.use((socket, next) => {
+        const session = socket.request.session;
+        if (session && session.authenticated) {
+          next();
+        } else {
+          next(new Error('unauthorized'));
+          console.log('Unauthorized session');
+        }
+      });
+
       const onConnection = (socket: Socket) => {
+        console.log(socket.request.session);
         registerTableEvents(this.io, socket);
       };
 
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import express from 'express';
 import { createServer } from 'http';
 
 import SocketServer from './SocketServer';
+import session, { Session } from 'express-session';
+
+declare module 'http' {
+  interface IncomingMessage {
+    session: Session & {
+      authenticated: boolean;
+    };
+  }
+}
 
 const app = express();
 const httpServer = createServer(app);
@@ -12,6 +21,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const sessionMiddleware = session({
+  secret: 'changeit',
+  resave: false,
+  saveUninitialized: false,
+});
+
+app.use(sessionMiddleware);
+
+// Authenticate the session
+app.post('/login', (req, res) => {
+  req.session.authenticated = true;
+  res.redirect('/');
+});
+
 httpServer.listen(3000);
 
-SocketServer.initialize(httpServer);
+SocketServer.initialize(httpServer, sessionMiddleware);
